7916e0dc8d23e4cd14d30378902c6829f334c054
Matthew Nessworthy <matthew.nessworthy@gmail.com>
2024-04-30 20:45:56 +0200

    The broken bit

---
+++ app/Filament/Resources/PageResource.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PageResource\Pages;
+use App\Models\Page;
+use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Wizard;
+use Filament\Forms\Components\Wizard\Step;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class PageResource extends Resource
+{
+    protected static ?string $model = Page::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                \Filament\Forms\Components\Builder::make('content_blocks')
+                    ->blocks([
+                        Block::make('widget')
+                            ->schema([
+                                Wizard::make([
+                                    Step::make('Widget')
+                                        ->schema([
+                                            Radio::make('widget')
+                                                ->options(
+                                                    [
+                                                        'widget-1' => 'Widget 1',
+                                                        'widget-2' => 'Widget 2',
+                                                    ]
+                                                ),
+                                        ]),
+                                    Step::make('Fields')
+                                        ->schema(
+                                            function (Get $get) {
+                                                $widget = (string) $get('widget');
+
+                                                return match ($widget) {
+                                                    'widget-1' => [
+                                                        Select::make('broken_js_select_box')
+                                                            ->multiple()
+                                                            ->options([
+                                                                'option-1' => 'Option 1',
+                                                                'option-2' => 'Option 2',
+                                                                'option-3' => 'Option 3',
+                                                            ]),
+                                                    ],
+                                                    default => [
+                                                        TextInput::make('limit')
+                                                            ->integer()
+                                                            ->minValue(1)
+                                                            ->maxValue(2),
+                                                    ],
+                                                };
+                                            }
+                                        ),
+                                ]),
+                            ]),
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPages::route('/'),
+            'create' => Pages\CreatePage::route('/create'),
+            'edit' => Pages\EditPage::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/migrations/2024_04_30_182910_create_pages_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->json('content_blocks')->nullable();
+            $table->timestamps();
+        });
+    }
+};
